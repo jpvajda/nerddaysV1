1cfca0422973bb8917ffa540150d389971d6057e
John P Vajda <jvajda@newrelic.com>
2020-10-08 18:06:46 -0600

    initial commit

---
+++ nerdlets/nerddaysv1-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Nerddaysv1NerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, nerddaysv1-nerdlet Nerdlet!</h1>;
+    }
+}
